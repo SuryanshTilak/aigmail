e553488532364fe84ef2dad54bd991ce8732c38f
Suryansh Tilak <suryansh.tilak@gmail.com>
2024-10-20 17:06:30 +0530

    1-Clerk done

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+
+export default async function Home() {
+  return(<> <h1 className="bg-red-600">  HI</h1>
+  <Button> yo</Button>
+  </>
+  )
+
+}
